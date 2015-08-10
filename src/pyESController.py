2dbf2fe6aaf0443edc8f23c9f40b92511f987b3f
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2015-08-10 17:33:29 +0300

    safety commit

--- src/pyESController.py
+++ src/pyESController.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
+import csv
+import unicodedata
 
-es = Elasticsearch('<ES-Endpoint>')
+
 
 
 #body got from request tab in kibana
-body = {
+
+
+
+def queryConstructor(ordering, tstart, tstop, queryString):
+  defaultBody= {
   "size": 500,
   "sort": {
-    "@timestamp": "desc"
+    "@timestamp": ordering
   },
   "query": {
     "filtered": {
       "query": {
         "query_string": {
-          "query": "host:*",
+          "query": queryString,
           "analyze_wildcard": True
         }
       },
@@ -24,76 +30,8 @@ body = {
             {
               "range": {
                 "@timestamp": {
-                  "gte": 1438900439450,
-                  "lte": 1438901339450
-                }
-              }
-            }
-          ],
-          "must_not": []
-        }
-      }
-    }
-  },
-  "highlight": {
-    "pre_tags": [
-      "@kibana-highlighted-field@"
-    ],
-    "post_tags": [
-      "@/kibana-highlighted-field@"
-    ],
-    "fields": {
-      "*": {}
-    }
-  },
-  "aggs": {
-    "2": {
-      "date_histogram": {
-        "field": "@timestamp",
-        "interval": "30s",
-        "pre_zone": "+03:00",
-        "pre_zone_adjust_large_interval":True,
-        "min_doc_count": 0,
-        "extended_bounds": {
-          "min": 1438900439450,
-          "max": 1438901339450
-        }
-      }
-    }
-  },
-  "fields": [
-    "*",
-    "_source"
-  ],
-  "script_fields": {},
-  "fielddata_fields": [
-    "@timestamp",
-    "received_at"
-  ]
-}
-
-
-testServiceType = {
-  "size": 500,
-  "sort": {
-    "@timestamp": "desc"
-  },
-  "query": {
-    "filtered": {
-      "query": {
-        "query_string": {
-          "query": "hostname:\"dice.cdh5.s2.internal\" AND serviceType:\"yarn\" ",
-          "analyze_wildcard": True
-        }
-      },
-      "filter": {
-        "bool": {
-          "must": [
-            {
-              "range": {
-                "@timestamp": {
-                  "gte": 1438939155342,
-                  "lte": 1438940055342
+                  "gte": tstart,
+                  "lte": tstop
                 }
               }
             }
@@ -112,22 +50,77 @@ testServiceType = {
     "@timestamp"
   ]
 }
+  return defaultBody
 
 
-#these are the metrics listed in the response JSON under "_source"
-metrics = ['message','type','@timestamp','host','job_id','hostname','AvailableVCores']
-
-res = es.search(index="logstash-*",body=testServiceType)
-print("%d documents found" % res['hits']['total'])
-for doc in res['hits']['hits']:
-  for met in metrics:
-    #prints the values of the metrics defined in the metrics list
-    print("%s) %s" % (doc['_id'], doc['_source'][met]))
 
 
-# def queryESCore(es,body,index="logstash-*"):
-#   res = es.search(index,body)
-#   print("%d documents found" % res['hits']['total'])
-#   for doc in res['hits']['hits']:
-#     print("%s) %s" % (doc['_id'], doc['_source']['AvailableVCores']))
 
+
+
+def queryESCore(queryString, all=False):
+  #these are the metrics listed in the response JSON under "_source"
+  metrics = ['message','type','@timestamp','host','job_id','hostname','AvailableVCores']
+  res = es.search(index="logstash-*",body=queryString)
+  #print res
+  print("%d documents found" % res['hits']['total'])
+  for doc in res['hits']['hits']:
+    if all == False:
+      for met in metrics:
+      #prints the values of the metrics defined in the metrics list
+        print("%s) %s" % (doc['_id'], doc['_source'][met]))
+    else:
+      #get all terms from query
+      for terms in doc['_source']:
+      #prints the values of the metrics defined in the metrics list
+        print("%s) %s %s" % (doc['_id'],terms,  doc['_source'][terms]))
+
+def queryESCoreCSV(queryString, all=False):
+  #these are the metrics listed in the response JSON under "_source"
+  metrics = ['message','type','@timestamp','host','job_id','hostname','AvailableVCores']
+  res = es.search(index="logstash-*",body=queryString)
+  #print res
+  #print("%d documents found" % res['hits']['total'])
+  #get results
+  results = res['hits']['hits']
+  termsList = []
+  #print results
+  for doc in results:
+    for terms in doc['_source']:
+      termsList.append(terms)
+  termsUniqueList = list(set(termsList)) #retain only the unique value from the list of terms
+  print termsUniqueList  #####
+
+  with open('output.csv','wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',' #can be changed to ',' for standard csv
+      ,quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    #Create headers
+    filewriter.writerow(termsUniqueList)    #change the column labels here
+    for hit in results:
+      #print hit
+      for terms in termsList:
+        #print terms
+        #print hit['_source'][terms].decode('utf-8')
+        colList = []
+        try: 
+          col =  hit['_source'][terms]
+        except Exception, e:
+          #print "What?"
+          col = "a "
+        colList.append(col)
+        #print colList
+        
+    filewriter.writerow(colList)
+
+
+      
+
+if __name__=='__main__':
+  #Elastic search endpoint
+  es = Elasticsearch('109.231.126.38')
+  test = queryConstructor("desc",1438939155342,1438940055342,"hostname:\"dice.cdh5.s3.internal\" AND serviceType:\"yarn\"")
+  print test
+
+  queryESCore(test, True)
+  #queryESCoreCSV(test, True)
