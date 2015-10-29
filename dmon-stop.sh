8434b8ecf5117f4bcb63d2e9923420956ba84815
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2015-10-29 17:42:39 +0200

    new stopping script

--- dmon-stop.sh
+++ dmon-stop.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-killall -9 python
+DIR=
+if [ $ARCH == "Linux" ]; then
+   DIR=`readlink -f "$( dirname "$0" )"`
+elif [ $ARCH == "Darwin" ]; then
+   CMD="import os, sys; print os.path.realpath(\"$( dirname $0 )\")"
+   DIR=`python -c "$CMD"`
+fi
+
+echo "Stopping Kibana ..."
+kill -9  $(cat $DIR/src/pid/kibana.pid)
+echo "Stopping Logstash Server ..."
+kill -9  $(cat $DIR/src/pid/logstash.pid)
+echo "Stopping ElasticSearch ..."
+kill -9  $(cat $DIR/src/pid/elasticsearch.pid)
+
+echo "Stopping D-Mon"
+killall -9 python #TODO need more elegant solution
+
+echo "D-Mon Stopped"
