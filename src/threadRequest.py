36c30f10551be62615b65265c625aacf1cb07287
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2015-11-24 01:33:40 +0200

    first working version of threadedRequest methods

--- src/threadRequest.py
+++ src/threadRequest.py
@@ -4,15 +4,16 @@ import time
 import requests
 
 
-class getThread (threading.Thread):
+class getThread(threading.Thread):
     def __init__(self, threadID, resource, q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.resource = resource
         self.q = q
+
     def run(self):
         print "Starting thread " + str(self.threadID)
-        tRequest(self. resource, self.q)
+        tRequest(self.resource, self.q)
         print "Exiting thread " + str(self.threadID)
 
 
@@ -23,14 +24,15 @@ class postThread(threading.Thread):
         self.resource = resource
         self.q = q
         self.payload = payload
+
     def run(self):
         print "Starting thread  " + str(self.threadID)
-        tRequestPost(self. resource, self.q, self.payload)
+        tRequestPost(self.resource, self.q, self.payload)
         print "Exiting thread " + str(self.threadID)
 
 
-def tRequest(resource,  q):
-    response ={}
+def tRequest(resource, q):
+    response = {}
     statusCode = {}
     data = {}
     while not exitFlag:
@@ -40,7 +42,7 @@ def tRequest(resource,  q):
             try:
                 r = requests.get(resourceURI, timeout=2)
                 data = r.json()
-                response['Node']= resourceURI
+                response['Node'] = resourceURI
                 response['StatusCode'] = r.status_code
                 response['Data'] = data
             except requests.exceptions.Timeout:
@@ -56,129 +58,86 @@ def tRequest(resource,  q):
         time.sleep(1)
 
 
-def tRequestPost(resource, q, payload):
-    response ={}
+def tRequestPost(resource, q, payload=None):
+    response = {}
     statusCode = {}
+    data = {}
     while not exitFlag:
         queueLock.acquire()
         if not workQueue.empty():
-            data = q.get()
-            r = requests.post(resource, data=payload, timeout=2)
-            statusCode[resource] = r.status_code
+            resourceURI = q.get()
+            response['Node'] = resourceURI
+            try:
+                if payload is None:
+                    r = requests.post(resourceURI, timeout=2)
+                else:
+                    r = requests.post(resourceURI, data=payload, timeout=2)
+                if r.headers['Content-Type'] == 'application/json':
+                    data = r.json()
+                else:
+                    data = r.text
+                response['StatusCode'] = r.status_code
+                response['Data'] = data
+            except requests.exceptions.Timeout:
+                response['StatusCode'] = 408
+                response['Data'] = data
+
+            NodeResponses.append(response)
             queueLock.release()
-            print "%s response %s" % (data, r.json()['Node'])
+            print "%s response %s" % (resourceURI, data)
         else:
             queueLock.release()
         time.sleep(1)
 
+if __name__ == '__main__':
+    resourceList = ['http://127.0.1.1:5000/agent/v1/node', 'http://127.0.0.1:5000/agent/v1/node']
 
-# class ParallelRequest():  # TODO: finish parallel request class
-#     #exitFlag = 0
-#     #threadID = 1
-#     #threads = []
-#     #queueLock = threading.Lock()
-#
-#
-#     def __init__(self, threadCount, queueSize, threadID):
-#         self.threadCount = threadCount
-#         self.queueSize = queueSize
-#         self.threadID = threadID
-#
-#     def executeGet(self, nodeList, resource):
-#         for node in nodeList:
-#             workQueue = Queue.Queue(self.queueSize)
-#             thread = getThread(self.threadID, node, resource, workQueue)
-#             thread.start()
-#             threads.append(thread)
-#             self.threadID += 1
-#
-#         #Fill the queue
-#         queueLock.acquire()
-#         for node in nodeList:
-#             workQueue.put(node)
-#         queueLock.release()
-#
-#         # Wait for queue to empty
-#         while not workQueue.empty():
-#             pass
-#
-#         # Notify threads it's time to exit
-#         exitFlag = 1
-#
-#         # Wait for all threads to complete
-#         for t in threads:
-#             t.join()
-#         print "Exiting Main Thread"
-#         return 'get'
-#
-#     def executePut(self, nodeList, resource, content):
-#         return 'put'
-#
-#     def executePost(self, nodeList, resource, content):
-#         return 'post'
-#
-#     def executeDelete(self, nodeList, resource):
-#         return 'delete'
+    queueLock = threading.Lock()
+    workQueue = Queue.Queue(len(resourceList))
 
+    threads = []
+    threadID = 1
+    exitFlag = 0
 
+    # List containing Responses
+    NodeResponses = []
 
-# nodel = [1,2,3,4,5,6,8,7]
-# resourceURI = 'http://127.0.0.1:5000/agent/v1/node'
-# test = ParallelRequest(4,11)
-#
-#test.executeGet(nodel, resourceURI)
+    # Max number of threads set to 10
+    threadList = []
+    if len(resourceList) < 10:
+        tIter = len(resourceList)
+    else:
+        tIter = 10
 
+    for x in range(0, tIter):
+        tStr = "Thread-%s" % x
+        x += 1
+        threadList.append(tStr)
 
-resourceList = ['http://127.0.1.1:5000/agent/v1/node', 'http://127.0.0.1:5000/agent/v1/node']
+    print threadList
+    # Create new threads
+    for tName in threadList:
+        thread = getThread(threadID, tName, workQueue)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
 
+    # Fill the queue
+    queueLock.acquire()
+    for word in resourceList:
+        workQueue.put(word)
+    queueLock.release()
 
-queueLock = threading.Lock()
-workQueue = Queue.Queue(len(resourceList))
+    # Wait for queue to empty
+    while not workQueue.empty():
+        pass
 
+    # Notify threads it's time to exit
+    exitFlag = 1
 
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
 
-threads = []
-threadID = 1
-exitFlag = 0
-
-# List containing Responses
-NodeResponses = []
-
-threadList = []
-if len(resourceList) < 10:
-    tIter = len(resourceList)
-else:
-    tIter = 10
-
-for x in range(0, tIter):
-    tStr = "Thread-%s" % x
-    x += 1
-    threadList.append(tStr)
-
-print threadList
-# Create new threads
-for tName in threadList:
-    thread = getThread(threadID, tName, workQueue)
-    thread.start()
-    threads.append(thread)
-    threadID += 1
-
-# Fill the queue
-queueLock.acquire()
-for word in resourceList:
-    workQueue.put(word)
-queueLock.release()
-
-# Wait for queue to empty
-while not workQueue.empty():
-    pass
-
-# Notify threads it's time to exit
-exitFlag = 1
-
-# Wait for all threads to complete
-for t in threads:
-    t.join()
-
-print NodeResponses
-print "Exiting Main Thread"
+    print NodeResponses
+    print "Exiting Main Thread"
