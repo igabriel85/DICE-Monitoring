ed98a1b9424207eb13ddf94c64314ba8e99ac9c5
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2015-12-02 15:19:49 +0200

    added new methods PUT, POST, DELETE

--- src/greenletThreads.py
+++ src/greenletThreads.py
@@ -1,6 +1,10 @@
 import gevent
 import gevent.queue
 import requests
+import json
+
+
+headers = {'content-type': 'application/json'}
 
 
 class GreenletRequests():
@@ -33,10 +37,11 @@ class GreenletRequests():
 
     def parallelPost(self, payload):
         queue = gevent.queue.Queue()
+        gList = []
+
         for i in self.resourceList:
             queue.put(i)
 
-        gList = []
         for t in range(len(self.resourceList)):
             gl = gevent.spawn(postRequest, queue, payload)
             gList.append(gl)
@@ -46,7 +51,54 @@ class GreenletRequests():
 
         return GreenletRequests.NodeResponsesPost
 
-    def reset(self): # TODO:  Check if reset is required for all attributes separately or general one is ok
+        if self.resourceList is dict:
+            for k,v in self.resourceList.iteritems():
+                print k
+                queue.put(k)
+
+            for k, v in self.resourceList.iteritems():
+                gl = gevent.spawn(postRequest, queue, v)
+                gList.append(gl)
+
+            print str(gList)
+            gevent.joinall(gList)
+
+            return GreenletRequests.NodeResponsesPost
+
+
+
+    def parallelPut(self, payload):
+        queue = gevent.queue.Queue()
+        for i in self.resourceList:
+            queue.put(i)
+
+        gList = []
+        for p in range(len(self.resourceList)):
+            gl = gevent.spawn(putRequest, queue, payload)
+            gList.append(gl)
+
+        print str(gList)
+        gevent.joinall(gList)
+
+        return GreenletRequests.NodeResponsesPut
+
+
+    def parallelDelete(self):
+        queue = gevent.queue.Queue()
+        for i in self.resourceList:
+            queue.put(i)
+
+        gList = []
+        for t in range(len(self.resourceList)):
+            gl = gevent.spawn(deleteRequest, queue)
+            gList.append(gl)
+
+        print str(gList)
+        gevent.joinall(gList)
+
+        return GreenletRequests.NodeResponsesDelete
+
+    def reset(self):  # TODO:  Check if reset is required for all attributes separately or general one is ok
         GreenletRequests.NodeResponsesGet = []
         GreenletRequests.NodeResponsesPost = []
         GreenletRequests.NodeResponsesPut = []
@@ -57,10 +109,10 @@ class GreenletRequests():
 
 def randomT(queue, name):
     while not queue.empty():
-        t = queue.get(timeout = 1)
+        t = queue.get(timeout=1)
         gevent.sleep(5)
-        print 'I am + '+name+ ' executing ' + str(GreenletRequests.ng)
-        GreenletRequests.ng+=1
+        print 'I am + ' + name + ' executing ' + str(GreenletRequests.ng)
+        GreenletRequests.ng += 1
         gevent.sleep(0)
 
 
@@ -93,16 +145,51 @@ def postRequest(queue, payload=None):
     response = {}
     statusCode = {}
     data = {}
-    while not queue.empty:
+    while not queue.empty():
         resourceURI = queue.get(timeout=1)
+        print payload
+        try:
+            if payload is None:
+                r = requests.post(resourceURI, timeout=20, headers=headers)
+            else:
+               # print "$$$$$$$$" + str(payload)
+                r = requests.post(resourceURI, data=json.dumps(payload), timeout=20, headers=headers)
+            if r.headers['Content-Type'] == 'application/json':
+                data = r.json()
+            else:
+                data = r.text
+            response['Node'] = resourceURI
+            response['StatusCode'] = r.status_code
+            response['Data'] = data
+        except requests.exceptions.Timeout:
+            response['Node'] = resourceURI
+            response['StatusCode'] = 408
+            response['Data'] = data
+        except requests.exceptions.ConnectionError:
+            response['Node'] = resourceURI
+            response['StatusCode'] = 404
+            response['Data'] = 'n/a'
+
+        GreenletRequests.NodeResponsesPost.append(response)
+        print 'Threaded POST with ID ' + str(GreenletRequests.np) + ' executed for ' + resourceURI
+        GreenletRequests.np += 1
+        gevent.sleep(0)
+
+
+def putRequest(queue, payload=None):
+    response = {}
+    statusCode = {}
+    data = {}
+    while not queue.empty():
+        resourceURI= queue.get(timeout=1)
         response['Node'] = resourceURI
         try:
             if payload is None:
-                r = requests.post(resourceURI, timeout=2)
+                r = requests.put(resourceURI, timeout=20)
             else:
-                r = requests.post(resourceURI, data=payload, timeout=2)
+                r = requests.put(resourceURI, data=payload, timeout=20)
             if r.headers['Content-Type'] == 'application/json':
-                data = r.json()
+                data = r.json
             else:
                 data = r.text
             response['StatusCode'] = r.status_code
@@ -116,18 +203,45 @@ def postRequest(queue, payload=None):
             response['Data'] = 'n/a'
 
         GreenletRequests.NodeResponsesPost.append(response)
-        print 'Threaded POST with ID '+str(GreenletRequests.np)+ ' executed for ' + resourceURI
-        GreenletRequests.np+=1
+        print 'Threaded PUT with ID ' + str(GreenletRequests.npo) + ' executed for ' + resourceURI
+        GreenletRequests.npo += 1
         gevent.sleep(0)
 
-# resourceList = ['http://109.231.121.135:5000/agent/v1/check', 'http://109.231.121.194:5000/agent/v1/check']
-# test = GreenletRequests(resourceList)
+
+def deleteRequest(queue):
+    response = {}
+    while not queue.empty():
+        resURI = queue.get(timeout=1)
+        try:
+            r = requests.delete(resURI, timeout=2)
+            data = r.json()
+            response['Node'] = resURI
+            response['StatusCode'] = r.status_code
+            response['Data'] = data
+        except requests.exceptions.Timeout:
+            response['Node'] = resURI
+            response['StatusCode'] = 408
+            response['Data'] = 'n/a'
+        except requests.exceptions.ConnectionError:
+            response['Node'] = resURI
+            response['StatusCode'] = 404
+            response['Data'] = 'n/a'
+
+        GreenletRequests.NodeResponsesGet.append(response)
+        print 'Threaded DELETE with ID ' + str(GreenletRequests.nd) + ' executed for ' + resURI
+        GreenletRequests.nd += 1
+        gevent.sleep(0)
+
+#resourceList = ['http://109.231.121.135:5000/agent/v1/check', 'http://109.231.121.194:5000/agent/v1/check']
+#resourceList = ['http://109.231.121.135:5000/agent/v1/deploy','http://109.231.121.134:5000/agent/v1/deploy','http://109.231.121.156:5000/agent/v1/deploy','http://109.231.121.194:5000/agent/v1/deploy']
+#test = GreenletRequests(resourceList)
 #
-# testG = test.parallelGet()
-# testP = test.parallelPost(None)
-#
-# print testG
-# print testP
+#testG = test.parallelGet()
+#ff = {"roles": ["hdfs"]}
+#testP = test.parallelPost(None)
+
+#print testG
+#print testP
 
 
 
