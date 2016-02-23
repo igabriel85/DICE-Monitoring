14e0f47f4f4f1e0a78997f8f0fc74be902fe9059
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-02-23 18:43:13 +0200

    fix logstash bash script

--- dmon-logstash/logstash-start.sh
+++ dmon-logstash/logstash-start.sh
@@ -22,7 +22,7 @@ if [ $# -eq 0 ]; then
         python dmon-logstash.py > log/dmon-logstash.out 2>&1 &
         echo $! > pid/dmon-logstash.pid
     echo "Finished"
-elif [ $2 == "stop" ]; then
+elif [[ $1 == "stop" ]]; then
     echo "Stopping dmon-logstash"
     if [ ! -f $DIR/src/pid/dmon-logstash.pid ]; then
         echo "No Logstash PID file found."
@@ -30,7 +30,7 @@ elif [ $2 == "stop" ]; then
     PID=`cat $DIR/pid/logstash.pid`
     kill -9  `cat $DIR/pid/logstash.pid`
     sleep 5
-    while [ kill -0  $PID ]
+    while  kill -0  $PID
         do
             kill -9  $PID
             sleep 1
