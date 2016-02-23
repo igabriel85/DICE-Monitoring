db9161bad451a34a228e09d0ed5e23caffc3428b
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-02-23 18:27:39 +0200

    Updated dmon-logstash bash script

--- dmon-logstash/logstash-start.sh
+++ dmon-logstash/logstash-start.sh
@@ -2,8 +2,10 @@
 
 ARCH=`uname -s`
 DIR=
+PID=
 RE='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
 RENR='^[0-9]+$'
+
 
 if [ $ARCH == "Linux" ]; then
    DIR=`readlink -f "$( dirname "$0" )"`
@@ -20,6 +22,22 @@ if [ $# -eq 0 ]; then
         python dmon-logstash.py > log/dmon-logstash.out 2>&1 &
         echo $! > pid/dmon-logstash.pid
     echo "Finished"
+elif [ $1 == "stop" ]; then
+    echo "Stopping dmon-logstash"
+    if [ ! -f $DIR/src/pid/dmon-logstash.pid ]; then
+        echo "No Logstash PID file found."
+    fi
+    PID=`cat $DIR/pid/dmon-logstash.pid`
+    kill -9  `cat $DIR/pid/dmon-logstash.pid`
+    sleep 5
+    while [ kill -0  $PID ]
+        do
+            kill -9  $PID
+            sleep 1
+        done
+    echo "Stopped logstash server"
+    killall -9 python
+    echo "Stopped dmon-logstash"
 else
-   echo "DMON-logstash does not support commandline arguments!"
+   echo "DMON-logstash does not support this command line argument!"
 fi
