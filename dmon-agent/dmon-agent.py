1dee9d1c57b2f3781da9f767cd4743db6f4a4a96
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2016-04-06 23:53:22 +0300

    fixed invalid port and replaced popen with call

--- dmon-agent/dmon-agent.py
+++ dmon-agent/dmon-agent.py
@@ -517,4 +517,4 @@ if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.DEBUG)
     log.addHandler(handler)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5222, debug=True)
